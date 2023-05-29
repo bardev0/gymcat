fb05d740af1b0f9fa7752d41e5f9585baf02743a
GregD <test@gmail.com>
2023-05-29 15:17:13 +0200

    added workout class

--- src/import_csv.ts
+++ src/import_csv.ts
@@ -10,3 +10,52 @@ const pathCsv = "workouts_to_enter.csv";
 const csvFile = readCSV(pathCsv);
 
 console.log(csvFile);
+
+class Workout {
+  date: string;
+  timeStart: string;
+  timeEnd: string;
+  muscleGroup: string;
+  exerciseName: string;
+  exerciseMultiplier: number;
+  series: number;
+  repetitions: number;
+  totalSeries: number | undefined;
+  totalExercice: number | undefined;
+  totalDay: number | undefined;
+  rest?;
+
+  constructor(
+    date: string,
+    timeStart: string,
+    timeEnd: string,
+    muscleGroup: string,
+    exerciseName: string,
+    exerciseMultiplier: number,
+    series: number,
+    repetitions: number,
+    totalSeries: number,
+    totalExercice: number,
+    totalDay: number,
+    rest: any
+  ) {
+    this.date = date;
+    this.timeStart = timeStart;
+    this.timeEnd = timeEnd;
+    this.muscleGroup = muscleGroup;
+    this.exerciseName = exerciseName;
+    this.exerciseMultiplier = exerciseMultiplier;
+    this.series = series;
+    this.repetitions = repetitions;
+    this.totalSeries = totalSeries;
+    this.totalExercice = totalExercice;
+    this.totalDay = totalDay;
+    this.rest = rest;
+  }
+}
+//split file into arrays with each indivdual workout being on object
+//remove whitespce characters
+// csvFile.replace(/\n/, "")
+// csvFile.replace(/\r/, "")
+// const workoutList = csvFile.split(/2{\d}/)
+console.log(csvFile)
