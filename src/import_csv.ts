a3f1cf8dd435d27210a086339f36e551f8d262b9
greg <hzststhz@outlook.com>
2023-05-29 23:27:08 +0200

    split classes, created Interfaces for them, refactored variables

--- src/import_csv.ts
+++ src/import_csv.ts
@@ -9,82 +9,122 @@ const pathCsv = "workouts_to_enter.csv";
 let csvFile = readCSV(pathCsv);
 
 // trzeba podzielic workout od samych cwiczen
+
+interface IRow {
+  repetitions: number;
+  muscleGroup: string;
+  exerciseName: string;
+  exerciseMultiplier: number;
+  seriesNumber: number;
+  weight: number;
+  totalExercise: number;
+}
+class Row implements IRow {
+  repetitions: number;
+  muscleGroup: string;
+  exerciseName: string;
+  exerciseMultiplier: number;
+  seriesNumber: number;
+  weight: number;
+  totalExercise: number;
+
+  constructor(
+    seriesNumber: number,
+    muscleGroup: string,
+    exerciseName: string,
+    exerciseMultiplier: number,
+    repetitions: number,
+    weight: number
+  ) {
+    this.repetitions = repetitions;
+    this.muscleGroup = muscleGroup;
+    this.exerciseName = exerciseName;
+    this.exerciseMultiplier = exerciseMultiplier;
+    this.seriesNumber = seriesNumber;
+    this.weight = weight;
+    this.totalExercise = 0;
+  }
+}
+
+interface ISeria {
+  csvInRows: Array<IRow>;
+}
+class Seria implements ISeria {
+  csvInRows: Array<IRow>;
+
+  constructor() {
+    this.csvInRows = [];
+  }
+
+  addRow(row: IRow) {
+    this.csvInRows.push(row);
+  }
+}
+
 class Workout {
   date: string;
   timeStart: string;
   timeEnd: string;
-  muscleGroup: string;
-  exerciseName: string;
-  exerciseMultiplier: number;
-  series: number;
-  repetitions: number;
-  totalSeries: number | undefined;
-  totalExercice: number | undefined;
   totalDay: number | undefined;
-  rest?;
+  listaSerii: Array<ISeria>;
 
   constructor(
     date: string,
     timeStart: string,
     timeEnd: string,
-    muscleGroup: string,
-    exerciseName: string,
-    exerciseMultiplier: number,
-    series: number,
-    repetitions: number,
-    totalSeries: number,
-    totalExercice: number,
-    totalDay: number,
-    rest: any
+    totalDay: number
   ) {
     this.date = date;
     this.timeStart = timeStart;
     this.timeEnd = timeEnd;
-    this.muscleGroup = muscleGroup;
-    this.exerciseName = exerciseName;
-    this.exerciseMultiplier = exerciseMultiplier;
-    this.series = series;
-    this.repetitions = repetitions;
-    this.totalSeries = totalSeries;
-    this.totalExercice = totalExercice;
     this.totalDay = totalDay;
-    this.rest = rest;
+    this.listaSerii = [];
+  }
+
+  addSeries(obj: ISeria) {
+    this.listaSerii.push(obj);
   }
 }
-
 let regexDate = /(\d\d[\.]\d\d[\.]\d\d\d\d)/gm;
-
 let dates = csvFile.match(regexDate);
-// console.log(dates)
 
-let rows = csvFile.split("\n");
+let csvInRows = csvFile.split("\n");
 let hours: Array<string> = [];
 
-rows.forEach((row) => {
+csvInRows.forEach((row) => {
   if (row.match(/\d\d[\:]\d\d/g)) {
-    // console.log(row)
     hours.push(row);
   }
 });
 
-// console.log(hours)
+console.log(hours);
+
+
+// co jezeli jestem w stanie podzielic rows ze
+// jump to new row jezeli numer serii jest mniejszy niz poprzeni numer
 let pureCSV = csvFile.replace(/\s/gm, ",");
 pureCSV = pureCSV + "00.00.0000,,,";
+let wszystkieSerie = /(?<=total,,)(.+?)(?=\d\d[\.])/gm;
+let formatedSerie = pureCSV.match(wszystkieSerie);
 
-// console.log(pureCSV) /
-let seriesRx = /(?<=total,,)(.+?)(?=\d\d[\.])/gm;
-let serie = pureCSV.match(seriesRx);
-
-let decons: any = [];
-let singleSei = [];
-serie?.forEach((seria) => {
-  singleSei = seria.split(/[\,]/g);
-  decons.push(singleSei);
+let workout: any = [];
+let singleSei: any = [];
+formatedSerie?.forEach((ser) => {
+  singleSei = ser.split(/[\,]/g);
+  workout.push(singleSei);
   singleSei = [];
 });
 
-decons.forEach( (wok: any) => {
-	wok.forEach((sig: any) => { console.log(sig) } )
-}
-)
-// jezeli index i+8 jest pusty to znaczy ze trzeba spierdalać
+// kazdy index nastepuje w odstepach co 9 !
+console.log(workout[13][0])
+console.log(workout[13][9])
+console.log(workout[13][1])
+console.log(workout[13][10])
+
+workout.forEach(( dane:any ) => { console.log(dane)})
+/** tesing clases
+let row = new Row(1,"biceps","dumbbell curls",2,10,12)
+let seria1 = new Seria()
+seria1.addRow(row)
+
+console.log(seria1) */
