fb24cae1155457991913f06fa82cdfdce2b62c56
GregD <test@gmail.com>
2023-05-29 13:46:56 +0200

    working csv import to string

---
+++ src/import_csv.ts
@@ -0,0 +1,12 @@
+import * as fs from "fs";
+
+function readCSV(path: string): string {
+  const rawData = fs.readFileSync(path, { encoding: "utf8" });
+  return rawData;
+}
+
+// console.log(process.cwd())
+const pathCsv = 'workouts_to_enter.csv'
+const csvFile = readCSV(pathCsv)
+
+console.log(csvFile)
